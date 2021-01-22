5e013b524a44dc4df7be8235378459be36156b19
wnstlr0615 <ryan0@kakao.com>
2021-01-22 09:43:04 +0900

    07.useState를 통해 컴포넌트에서 바뀌는 값 관리하기, 카운트 만들기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import './App.css'
-import Wrapper from './Wrapper';
-import Test from './Test';
+import Counter from './Counter';
 
 function App() {
   return (
-  <>
-    <Wrapper>
-        <Hello name="react" color="red" isSpecial/> {/*isSpecial={true} 와 같음*/}
-        <Hello  color="pink"/>
-    </Wrapper>
-    <Test/>
-  </>
+      <Counter/>
   );
 }
 
