9739608099d4d073ca72163e1c2c4c33529c6ccc
wnstlr0615 <ryan0@kakao.com>
2021-01-24 15:10:48 +0900

    16.useEffect를 사용하여 마운트언마운트업데이트시 할 작업 설정

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 
 function User({user, onRemove, onToggle}){
     const {username, email, id, active}=user;
+    /*useEffect(()=> {
+        console.log('컴포넌트가 화면에 나타남');
+        return()=>{
+            console.log('컴포넌트가 화면에서 사라짐');
+        }
+    },[]);*/
+    useEffect(()=>{
+        console.log('user 값이 설정됨')
+        console.log(user);
+        return()=>{
+            console.log('user 값이 바뀌기 전')
+            console.log(user);
+        }
+    },[user]);
     return(
         <div>
                 <b style={{
