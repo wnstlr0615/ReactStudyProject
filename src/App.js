8972f1fa23ed6937b3ce063b839c65be6c595909
wnstlr0615 <ryan0@kakao.com>
2021-01-26 19:11:40 +0900

    24. UserDispatch Context 만들기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useReducer, useMemo, useCallback} from 'react';
+import React, {useRef, useReducer, useMemo, useCallback, createContext} from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 import useInput from "./useInput";
@@ -56,7 +56,7 @@ function reducer(state, action){
 
 }
 
-
+export const UserDispatch=createContext(null);
 function App() {
     const [state, dispatch]=useReducer(reducer, initialState);
     const[form, onChange, reset]=useInput({
@@ -81,27 +81,17 @@ function App() {
         nextId.current+=1;
         reset();
     }, [username, email, reset]);
-    const onToggle=useCallback(id=>{
-        dispatch({
-            type:'TOGGLE_USER',
-            id
-        })
-    },[]);
-    const onRemove=useCallback(id=>{
-        dispatch({
-            type:'REMOVE_USER',
-            id
-        })
-    },[]);
+
 
     const count=useMemo(()=>countActiveUsers(users), [users]);
     return (
-        <>
-            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-            <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
-            <div>활성 사용자 수 {count}</div>
-        </>
-    );
+            <UserDispatch.Provider value={dispatch}>
+                <CreateUser username={username} email={email} onCreate={onCreate} onChange={onChange}/>
+                <UserList users={users} />
+                <div>활성 사용자 수 {count}</div>
+
+            </UserDispatch.Provider>
+     );
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,9 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useContext} from 'react';
+import {UserDispatch} from "./App";
 
-const User=React.memo(function User({user, onRemove, onToggle}){
+const User=React.memo(function User({user}){
     const {username, email, id, active}=user;
+    const dispatch=useContext(UserDispatch);
     /*useEffect(()=> {
         console.log('컴포넌트가 화면에 나타남');
         return()=>{
@@ -22,13 +24,21 @@ const User=React.memo(function User({user, onRemove, onToggle}){
                     color:active&& 'green',
                     cursor:'pointer'
             }}
-            onClick={()=>onToggle(id)}
-            >{username}</b><span>({email})</span>
-                <button onClick={() => onRemove(id) } >삭제</button>
+            onClick={() => dispatch({
+                type: 'TOGGLE_USER',
+                id
+            })}
+            >
+                    {username}
+                </b><span>({email})</span>
+                <button onClick={() =>  dispatch({
+                    type: 'REMOVE_USER',
+                    id
+                })}>삭제</button>
         </div>
     );
 });
-function UserList({users, onRemove, onToggle}){
+function UserList({users}){
     return( 
         <div>
             {
@@ -37,8 +47,7 @@ function UserList({users, onRemove, onToggle}){
                     <User 
                     user={user} 
                     key={user.id} 
-                    onRemove={onRemove}
-                    onToggle={onToggle}
+
                     />
                     )
                 )
